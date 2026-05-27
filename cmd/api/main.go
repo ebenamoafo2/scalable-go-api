c429ada44f533d6938063df7397afdbbad2652ec
Ebenezer Ofori Amoafo <ebenamoafo222@gmail.com>
2026-05-27 10:07:55 +0000

    feat: integrate database connection and migration setup
    - Added PostgreSQL connection pooling with configurable parameters
    - Integrated database migration using golang-migrate
    - Introduced MovieModel with CRUD method placeholders in the data layer

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "0.0.1"
@@ -14,6 +25,12 @@ const version = "0.0.1"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -25,11 +42,60 @@ func main() {
 	//Declare an instance of the config struct
 	var cfg config
 
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+
 	flag.IntVar(&cfg.port, "port", 4000, "port to run the server on")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	// Read the connection pool settings from command-line flags into the config struct.
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			logger.Error(err.Error())
+		}
+	}(db)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+
+	migrationDriver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	migrationPath := os.Getenv("GREENLIGHT_MIGRATION_PATH")
+	migrator, err := migrate.NewWithDatabaseInstance(migrationPath,
+		"postgres", migrationDriver)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	err = migrator.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("database migrated")
 
 	app := &application{
 		config: cfg,
@@ -48,7 +114,33 @@ func main() {
 	//Start the server
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	//set the maximum number of connections in the idle connection pool
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	//set the maximum number of connections in the connection pool
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	//set the maximum amount of time a connection may be reused
+	db.SetConnMaxLifetime(cfg.db.maxIdleTime)
+
+	//cancel a contex with a 5-second timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	//ping the database to check if the connection is still valid
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/ebenamoafo2/scalable-go-api
 
 go 1.26.3
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
